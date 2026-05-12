2c7b8c32714e69d99e3f714107f3ab61977d4c36
Akshay Kumar S Kotur <155496902+akshay-kotur@users.noreply.github.com>
2026-05-12 17:32:11 +0530

    add lambda ingestion logic

--- lambda/lambda_ingestion.py
+++ lambda/lambda_ingestion.py
@@ -1,1 +1,41 @@
-# Lambda ingestion placeholder
+import json
+import boto3
+import random
+from datetime import datetime
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    
+    bucket_name = 'bison-de-project'  
+    
+    data = []
+    
+    for _ in range(5):
+        record = {
+            "user_id": random.randint(100, 999),
+            "event": random.choice(["view_product", "add_to_cart", "purchase"]),
+            "product_id": random.randint(1000, 2000),
+            "timestamp": datetime.utcnow().isoformat()
+        }
+        data.append(record)
+    
+    file_name = f"raw/data_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.json"
+    
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=json.dumps(data)
+    )
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Data uploaded successfully")
+    }
+
+
+
+
+
+
+    
